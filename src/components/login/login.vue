ba8768511336f71c727dd8ef57c6a9980a1e521a
Yqoo <yq910526@outlook.com>
2019-07-23 18:48:32 +0800

    login

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>登录页</div>
+    <div class="formBox">
+        <el-form ref="form" label-width="80px" size="medium">
+            <el-form-item label="用户名">
+                <el-input  prefix-icon="el-icon-user"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input prefix-icon="el-icon-lock"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button  type="primary">登录</el-button>
+                <el-button type="success">取消</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 <script>
 export default {
@@ -7,5 +20,15 @@ export default {
 }
 </script>
 <style lang="less">
-
+    @baseColor: #c4bfc2;
+    .formBox {
+        width: 500px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        border: 1px solid @baseColor;
+        padding: 50px;
+        border-radius: 5px;
+    }
 </style>
