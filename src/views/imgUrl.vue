7622228d40e740efef29312a75a0d6a42d1c8f6e
faithfully0xia <1123968770@qq.com>
2019-08-02 15:41:56 +0800

    图片主题

--- src/views/imgUrl.vue
+++ src/views/imgUrl.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class=''>
-    <el-input v-model="imgSrc" placeholder="请选择图片">
-      <el-button slot="append" icon="el-icon-search"></el-button>
-    </el-input>
+  <div class='imgUrl'>
     <el-upload
-      action="https://jsonplaceholder.typicode.com/posts/"
+      action="#"
       list-type="picture-card"
+      accept=".JPG,.JPEG,.PNG,.GIF,.SVG,.WEBP,.BMP"
+      :auto-upload = "false"
       :on-preview="handlePictureCardPreview"
       :on-remove="handleRemove">
       <i class="el-icon-plus"></i>
     </el-upload>
-    <el-dialog :visible.sync="dialogVisible">
-      <img width="100%" :src="dialogImageUrl" alt="">
-    </el-dialog>
+    <el-button size="small">保存</el-button>
   </div>
 </template>
 
@@ -23,21 +20,37 @@ export default {
     data() {
         return {
           imgSrc:'',
-          dialogImageUrl: '',
-          dialogVisible: false
         };
     },
     methods: {
+      handleSuccess(res, file){
+        console.log(res)
+      },
       handleRemove(file, fileList) {
         console.log(file, fileList);
       },
       handlePictureCardPreview(file) {
-        this.dialogImageUrl = file.url;
-        this.dialogVisible = true;
+        const h = this.$createElement;
+        this.$msgbox({
+          title:'壁纸查看',
+          message: h('div', {style: `background: url(${file.url}) 0% 0% /cover no-repeat;height: 300px;color: transparent;`},'1'),
+          showCancelButton: false,
+          showConfirmButton: false,
+          callback: action => {
+            return false;
+          }
+        });
       }
     }
 }
 </script>
 <style lang='less' scoped>
-
+  .el-button{
+    margin: 5px 0 0 0;
+  }
+  .imgUrl /deep/ .el-upload-list{
+    & > li {
+      margin: 0 8px 0 0;
+    }
+  }
 </style>
