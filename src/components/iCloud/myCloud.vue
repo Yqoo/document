53e2c2d70787dfc794dd6e0605d2d96113869b7a
Yqoo <yq910526@outlook.com>
2019-08-09 09:43:19 +0800

    我的云端重构

--- src/components/iCloud/myCloud.vue
+++ src/components/iCloud/myCloud.vue
@@ -1,3 +1,8 @@
+<!--
+ * @Date: 2019-07-30 09:10:35
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2019-08-09 09:42:30
+ -->
 <template>
   <div class='myCloud' v-drag :class="themeColorName">
     <boxTools class="theme-color moveBox" :style="themeColorStyle" :info="info" @windowsTools="windowsTools" :title="componentTitle"></boxTools>
@@ -14,52 +19,7 @@
         </div>
       </aside>
       <div class="rightContent">
-        <el-collapse v-model="activeCollapse" accordion>
-          <el-collapse-item name='1'>
-            <template slot="title">
-              <i class="el-icon-cloudy"></i>挚友云
-            </template>
-            <div class="cards slideInRight animated">
-              <el-card v-for="(c,index) in zyCloud" :key="index" shadow="hover" class="hvr-backward">
-                <i :class="c.imgurl"></i>
-                <div class="bottom">{{c.name}}</div>
-              </el-card>
-            </div>
-          </el-collapse-item>
-           <el-collapse-item>
-            <template slot="title">
-              <i class="el-icon-s-custom"></i>我的云端
-            </template>
-            <div class="cards slideInLeft animated">
-              <el-card v-for="(c,index) in myCloud" :key="index" shadow="hover" class="hvr-float">
-                <i :class="c.imgurl"></i>
-                <div class="bottom1">{{c.name}}</div>
-              </el-card>
-            </div>
-          </el-collapse-item>
-           <el-collapse-item>
-            <template slot="title">
-              <i class="el-icon-cloudy-and-sunny"></i>共享云端
-            </template>
-            <div class="cards slideInDown animated">
-              <el-card v-for="(c,index) in shareCloud" :key="index" shadow="hover" class="hvr-sink">
-                <i :class="c.imgurl"></i>
-                <div class="bottom1">{{c.name}}</div>
-              </el-card>
-            </div>
-          </el-collapse-item>
-           <el-collapse-item>
-            <template slot="title">
-              <i class="el-icon-partly-cloudy"></i>组织云端
-            </template>
-            <div class="cards slideInUp animated">
-              <el-card v-for="(c,index) in organizationCloud" :key="index" shadow="hover" class="hvr-forward">
-                <i :class="c.imgurl"></i>
-                <div class="bottom1">{{c.name}}</div>
-              </el-card>
-            </div>
-          </el-collapse-item>
-        </el-collapse>
+        <component :is="current"></component>
       </div>
     </section>
   </div>
@@ -68,10 +28,12 @@
 <script>
 import tools from  "@/assets/js/utils/tools.js";
 import boxTools from "@/views/boxTools";
+import iCloudIndex from "@/components/iCloud/iCloudIndex";
 export default {
     name:'myCloud',
     components:{
-      boxTools
+      boxTools,
+      iCloudIndex,
     },
     data() {
         return {
@@ -116,29 +78,7 @@ export default {
               ],
             }
           ],
-          activeCollapse:['1'],
-          zyCloud:[
-            {name:'我的桌面',imgurl:'el-icon-s-platform'},
-            {name:'我的文档',imgurl:'el-icon-folder'},
-            {name:'加密云端',imgurl:'el-icon-folder'},
-            {name:'我的共享',imgurl:'el-icon-user'},
-            {name:'接收共享',imgurl:'el-icon-sort'},
-            {name:'我的分享',imgurl:'el-icon-share'},
-            {name:'接收分享',imgurl:'el-icon-folder-checked'},
-            {name:'备份中心',imgurl:'el-icon-folder-checked'},
-          ],
-          myCloud:[
-            {name:'软件区',imgurl:'el-icon-mobile'},
-            {name:'文档区',imgurl:'el-icon-mobile'},
-            {name:'娱乐区',imgurl:'el-icon-mobile'},
-          ],
-          shareCloud:[
-            {name:'开发部',imgurl:'el-icon-mobile'},
-            {name:'学习区',imgurl:'el-icon-mobile'},
-          ],
-          organizationCloud:[
-            {name:'挚友',imgurl:'el-icon-office-building'}
-          ],
+          current:'iCloudIndex',//初始进入myCloud 默认右侧显示
         };
     },
     methods:{
@@ -211,28 +151,6 @@ export default {
         box-sizing: border-box;
       }
     }
-  }
-  .cards{
-    display:flex;
-    flex-flow: row wrap;
-    & .el-card {
-      margin: 5px 10px;
-      cursor: pointer;
-      width:80px;
-      height: 80px;
-      & i{
-        font-size: 40px;
-      }
-    }
-  }
-  .bottom {
-    font-size: 10px;
-    width: 50px;
-    text-align: left;
-  }
-  .bottom1 {
-    font-size:10px;
-    text-align:center
   }
   .clearfix:before,
   .clearfix:after {
