0fcd7b86c6f40681470e0eb8d98fd0eba604786c
faithfully0xia <1123968770@qq.com>
2019-08-09 09:24:34 +0800

    桌面图标

---
+++ src/components/files/files.vue
@@ -0,0 +1,17 @@
+<!-- 我的云端：顶部工具栏 -->
+<template>
+  <div class='fileUtils'></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
