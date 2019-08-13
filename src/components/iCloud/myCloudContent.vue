037971bfd10bb68fd352cf65005b49ff977b023a
faithfully0xia <1123968770@qq.com>
2019-08-13 10:05:13 +0800

    我的云端

---
+++ src/components/iCloud/myCloudContent.vue
@@ -0,0 +1,19 @@
+<!-- 我的云端：内容 -->
+<template>
+  <div class='myCloudContent'>
+      我的云端内容页
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+</style>
