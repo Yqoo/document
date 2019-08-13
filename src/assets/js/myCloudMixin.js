2ee744cb6945eeade56d5a8c3c84fb69e4146b2f
faithfully0xia <1123968770@qq.com>
2019-08-13 16:49:36 +0800

    我的云端

--- src/assets/js/myCloudMixin.js
+++ src/assets/js/myCloudMixin.js
@@ -6,6 +6,13 @@ export const myCloudMixin = {
         //点击右侧内容，切换工具栏
         clickBlock( tag ) {
             this.$emit('changeUtils', tag);
+        },
+        //单击（文件夹）时，有一个边框
+        activeCard(item, dataArr) {
+            for(let i=0; i< dataArr.length; i++){
+                dataArr[i].active = false;
+            }
+            item.active = true;
         }
     }
 }
---
+++ src/views/spaceProgress.vue
@@ -0,0 +1,55 @@
+<!-- 内存空间比例   进度条 -->
+<template>
+  <div class='spaceProgress clearfix'>
+    <span>已用空间：</span>
+    <el-progress :stroke-width="10" :percentage="percentage"></el-progress>
+    <span>总空间：{{totalSpace}}G</span>
+    <span>可用空间：{{avaliableSpace}}G</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['avaliableSpace', 'totalSpace'],
+  computed: {
+    percentage() {
+      return (this.avaliableSpace * 1 / this.totalSpace * 100).toFixed(0) * 1;
+    }
+  },
+  data () {
+    return {
+    };
+  },
+  methods: {}
+}
+
+</script>
+<style lang='less' scoped>
+.spaceProgress{
+  font-size: 12px;
+  & span{
+    float: left;
+  }
+  & span:nth-of-type(1){
+    width: 15%;
+  }
+  & span:nth-of-type(2){
+    margin-right: 2%;
+    width: 20%;
+  }
+  & span:nth-of-type(3){
+    width: 23%;
+  }
+  & .el-progress{
+    float: left;
+    width: 40%;
+    top: 3px;
+  }
+  & /deep/ .el-progress-bar__outer, & /deep/ .el-progress-bar__inner{
+    border-radius: 0;
+  }
+  & /deep/ .el-progress__text{
+    font-size: 12px !important;
+  }
+}
+</style>
