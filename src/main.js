ae5fd97e8eaad26041dcec4989b6a673f1530fd1
faithfully0xia <1123968770@qq.com>
2019-08-21 10:40:32 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -34,18 +34,18 @@ axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 Vue.prototype._ = _;
 Vue.prototype._getThemeColor = tools.getThemeColor;
 Vue.prototype._getPosition = tools.getPosition;
-// router.beforeEach((to,from,next) => {//全局路由守卫
-//   if(to.path === '/login'){
-//     next()
-//   } else {
-//     let token = localStorage.getItem('loginToken');
-//     if( token === null || token === ''){
-//       next({path: '/login'})
-//     } else{
-//       next();
-//     }
-//   }
-// })
+router.beforeEach((to,from,next) => {//全局路由守卫
+  if(to.path === '/login'){
+    next()
+  } else {
+    let token = localStorage.getItem('loginToken');
+    if( token === null || token === ''){
+      next({path: '/login'})
+    } else{
+      next();
+    }
+  }
+})
 Vue.directive('drag',{
   inserted:function( el,bilding ){
     let op = el;
