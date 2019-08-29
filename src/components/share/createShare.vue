2b71ea1178533bfb32ad297734b84d7cd170e3c2
faithfully0xia <1123968770@qq.com>
2019-08-29 18:34:25 +0800

    分享

--- src/components/share/createShare.vue
+++ src/components/share/createShare.vue
@@ -12,53 +12,57 @@
         <img src="@/assets/image/icons/fileIcons/myshare.png">
         <span>创建分享</span>
     </div>
-    <div class="resourceName">
-        <img src="@/assets/image/icons/fileIcons/attribute.png">
-        <span>分享资源</span>
-        <span class="name">
-            <img :src="require(`@/assets/image/icons/${info.img}`)">
-            {{info.name}}
-        </span>
+    <div v-if="!showLink">
+        <div class="resourceName">
+            <img src="@/assets/image/icons/fileIcons/attribute.png">
+            <span>分享资源</span>
+            <span class="name">
+                <img :src="require(`@/assets/image/icons/${info.img}`)">
+                {{info.name}}
+            </span>
+        </div>
+        <el-menu mode="horizontal">
+            <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/type.png"/>分享类型</el-menu-item>
+        </el-menu>
+        <div class="shareType">
+            <p><el-radio v-model="shareType" label="1">有提取码</el-radio><span>仅限拥有提取码者可查看，更加隐私安全</span></p>
+            <p><el-radio v-model="shareType" label="0">无提取码</el-radio><span>无需提取码，仅限拥有链接的用户可以查看</span></p>
+        </div>
+        <el-menu mode="horizontal">
+            <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/power.png"/>分享权限</el-menu-item>
+            <el-menu-item><el-checkbox v-model="checkAllLimit" @change="handleAllLimitChange">所有</el-checkbox></el-menu-item>
+            <el-menu-item><el-checkbox v-model="checkDialy" @change="(val)=>handlePartChange(val,'daily')">日常操作</el-checkbox></el-menu-item>
+            <el-menu-item><el-checkbox v-model="checkOnline" @change="(val)=>handlePartChange(val,'online')">在线操作</el-checkbox></el-menu-item>
+            <el-menu-item><el-checkbox v-model="checkForward" @change="(val)=>handlePartChange(val,'forward')">转发操作</el-checkbox></el-menu-item>
+        </el-menu>
+        <el-checkbox-group v-model="checkLimits" @change="handleCheckedChange">
+            <el-checkbox v-for="item in shareLimits" :label="item.id" :key="item.name">{{item.label}}</el-checkbox>
+        </el-checkbox-group>
+        <el-menu mode="horizontal">
+            <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/calendar.png"/>分享期限</el-menu-item>
+        </el-menu>
+        <div class="shareDate">
+            <el-radio v-model="shareDate" label="forever">永久有效</el-radio>
+            <el-radio v-model="shareDate" label="1">1天</el-radio>
+            <el-radio v-model="shareDate" label="3">3天</el-radio>
+            <el-radio v-model="shareDate" label="7">7天</el-radio>
+            <el-radio v-model="shareDate" label="custom">自定义</el-radio>
+        </div>
+        <el-menu mode="horizontal">
+            <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/line.png"/>访问次数</el-menu-item>
+        </el-menu>
+        <div class="shareDate">
+            <el-radio v-model="visitTime" label="forever">不限次数</el-radio>
+            <el-radio v-model="visitTime" label="10">10次</el-radio>
+            <el-radio v-model="visitTime" label="30">30次</el-radio>
+            <el-radio v-model="visitTime" label="50">50次</el-radio>
+            <el-radio v-model="visitTime" label="custom">自定义</el-radio>
+        </div>
     </div>
-    <el-menu mode="horizontal">
-        <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/type.png"/>分享类型</el-menu-item>
-    </el-menu>
-    <div class="shareType">
-        <p><el-radio v-model="shareType" label="1">有提取码</el-radio><span>仅限拥有提取码者可查看，更加隐私安全</span></p>
-        <p><el-radio v-model="shareType" label="0">无提取码</el-radio><span>无需提取码，仅限拥有链接的用户可以查看</span></p>
-    </div>
-    <el-menu mode="horizontal">
-        <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/power.png"/>分享权限</el-menu-item>
-        <el-menu-item><el-checkbox v-model="checkAllLimit" @change="handleAllLimitChange">所有</el-checkbox></el-menu-item>
-        <el-menu-item><el-checkbox v-model="checkDialy" @change="(val)=>handlePartChange(val,'daily')">日常操作</el-checkbox></el-menu-item>
-        <el-menu-item><el-checkbox v-model="checkOnline" @change="(val)=>handlePartChange(val,'online')">在线操作</el-checkbox></el-menu-item>
-        <el-menu-item><el-checkbox v-model="checkForward" @change="(val)=>handlePartChange(val,'forward')">转发操作</el-checkbox></el-menu-item>
-    </el-menu>
-    <el-checkbox-group v-model="checkLimits" @change="handleCheckedChange">
-        <el-checkbox v-for="item in shareLimits" :label="item.id" :key="item.name">{{item.label}}</el-checkbox>
-    </el-checkbox-group>
-    <el-menu mode="horizontal">
-        <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/calendar.png"/>分享期限</el-menu-item>
-    </el-menu>
-    <div class="shareDate">
-        <el-radio v-model="shareDate" label="forever">永久有效</el-radio>
-        <el-radio v-model="shareDate" label="1">1天</el-radio>
-        <el-radio v-model="shareDate" label="3">3天</el-radio>
-        <el-radio v-model="shareDate" label="7">7天</el-radio>
-        <el-radio v-model="shareDate" label="custom">自定义</el-radio>
-    </div>
-    <el-menu mode="horizontal">
-        <el-menu-item :disabled='true'><img src="@/assets/image/icons/fileIcons/line.png"/>访问次数</el-menu-item>
-    </el-menu>
-    <div class="shareDate">
-        <el-radio v-model="visitTime" label="forever">不限次数</el-radio>
-        <el-radio v-model="visitTime" label="10">10次</el-radio>
-        <el-radio v-model="visitTime" label="30">30次</el-radio>
-        <el-radio v-model="visitTime" label="50">50次</el-radio>
-        <el-radio v-model="visitTime" label="custom">自定义</el-radio>
-    </div>
+    <div v-if="showLink"></div>
     <div slot="footer" class="dialog-footer">
-        <el-button type='primary' size='small'>创建链接</el-button>
+        <el-button v-if='!showLink' type='primary' size='small' @click="createLink">创建链接</el-button>
+        <el-button v-if="showLink" type='primary' size='small'>复制链接与提取码</el-button>
         <el-button @click="closeDialog" size="small">关闭</el-button>
     </div>
   </el-dialog>
@@ -81,6 +85,7 @@ export default {
         checkLimits:[], //已勾选的权限
         shareDate:'1', //分享期限
         visitTime:'10', //访问次数
+        showLink:false, // 创建分享： true：分享页面  false：链接页面
         shareLimits:[
             {id:1,name:'new',label:'新建',type:'daily'},{id:2,name:'cut',label:'剪切',type:'daily'},{id:3,name:'copy',label:'复制',type:'daily'},{id:4,name:'paste',label:'粘贴',type:'daily'},{id:5,name:'rename',label:'重命名',type:'daily'},{id:6,name:'del',label:'删除',type:'daily'},
             {id:7,name:'preview',label:'在线预览',type:'online'},{id:8,name:'edit',label:'在线编辑',type:'online'},{id:9,name:'collaborative',label:'协同编辑',type:'online'},{id:10,name:'compress',label:'在线压缩',type:'online'},{id:11,name:'decompress',label:'在线解压',type:'online'},{id:12,name:'encryption',label:'文件加密',type:'online'},{id:13,name:'deciphering',label:'文件解密',type:'online'},
@@ -164,6 +169,9 @@ export default {
       },
       closeDialog(){ //关闭弹框
           this.$emit('closeDialog','showShare');
+      },
+      createLink(){
+          this.showLink = true;
       }
   },
   mounted(){
